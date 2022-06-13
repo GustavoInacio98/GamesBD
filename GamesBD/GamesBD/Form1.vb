b84ff977c8b52f8313106627b19da32b562143f5
unknown <gustavoinac@gmail.com>
2022-06-13 15:31:51 +0100

    Inicio do Form inicial de conexão

---
+++ GamesBD/GamesBD/Form1.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Data.SqlClient
+
+Private Sub TestDBConnection(ByVal dbServer As String, ByVal dbName As String,
+ByVal userName As String, ByVal userPass As String)
+
+    Dim CN As New SqlConnection("Data Source = " + dbServer + " ;" +
+"Initial Catalog = " + dbName + "; uid = " + userName + ";" +
+"password = " + userPass)
+
+    Try
+        CN.Open()
+        If CN.State = ConnectionState.Open Then
+            MsgBox("Successful connection to database " & CN.Database & " on the " & CN.DataSource &
+            " server", MsgBoxStyle.OkOnly, "Connection Test")
+        End If
+    Catch ex As Exception
+        MsgBox("FAILED TO OPEN CONNECTION TO DATABASE DUE TO THE FOLLOWING ERROR" & vbCrLf &
+        ex.Message, MsgBoxStyle.Critical, "Connection Test")
+    End Try
+    If CN.State = ConnectionState.Open Then
+        CN.Close()
+
+End Sub
+
+Public Class Form1
+
+End Class
